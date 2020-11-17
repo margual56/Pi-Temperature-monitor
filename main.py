bc6ab8a02e27bc383ae97b5553ddd012892637e7
Marcos Gutiérrez Alonso <margual56@gmail.com>
2020-11-17 01:47:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,21 @@ from flask import Flask, flash, redirect, render_template, request, session, url
 import os
 import json
 import subprocess
+import psutil
 
 app = Flask(__name__)
 
 
 def stats():
-    proc = subprocess.Popen(["sudo " + monPath], stdout=subprocess.PIPE, shell=True)
-    (out, err) = proc.communicate()
-    return out
+    memory = psutil.virtual_memory()
+    # Divide from Bytes -> KB -> MB
+    available = round(memory.available / 1024.0 / 1024.0, 1)
+    total = round(memory.total/1024.0/1024.0, 1)
+
+    res = os.popen('vcgencmd measure_temp').readline()
+    temp = res.replace("temp=", "").replace("'C\n", "")
+
+    return float(psutil.cpu_percent()), float(temp), float(available-total)
 
 
 @app.route("/")
@@ -19,33 +26,9 @@ def main_page():
 
 @app.route("/status", methods=['GET'])
 def step():
-    info = stats().split(";")
-    return jsonify({"cputemp": float(info[1]), "usage": float(info[2])})
-
-
-@app.route("/info")
-def reset_data():
-    return render_template('info.html', data=stats())
-
-
-# @app.route('/favicon.ico')
-# def favicon():
-#     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    usage, temp, ram = stats()
+    return jsonify({"cputemp": temp, "usage": usage, "ram": ram})
 
 
 if __name__ == "__main__":
-    # monPath = os.path.join(app.root_path, 'static', "monitor.sh")
-    monPath = "./monitor.sh"
-    if not os.path.exists(monPath):
-        with open(monPath, 'w') as f:
-            f.write("#!/bin/sh\n\n")
-            f.write("date=$(date '+%d/%m/%Y %H:%M:%S')\n")
-            f.write("temp=$(vcgencmd measure_temp | egrep -o '[0-9]*\\.[0-9]*')\n")
-            f.write("usage=$(top -bn 1 | head -n 3 | tail -n 1 | awk '{print 100-$8}')\n")
-            f.write("tmp=\"${date};${temp};${usage}\"\n")
-            f.write("echo $tmp")
-
-        os.system("chmod +x " + monPath)
-
-    app.static_folder = 'static'
     app.run('0.0.0.0', port=15050)
